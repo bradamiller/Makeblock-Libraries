e2261d04d08d8ac6fc75b0e09b72228ba1f59831
bradamiller <brad@bradhouse.com>
2020-10-08 18:44:08 -0700

    Added the Chassis class

---
+++ src/Chassis.cpp
@@ -0,0 +1,60 @@
+#include "Chassis.h"
+
+MeEncoderOnBoard left(SLOT2);
+MeEncoderOnBoard right(SLOT1);
+
+Chassis::Chassis() {}
+
+void Chassis::isrLeft() {
+  if (digitalRead(left.getPortB()) == 0) {
+    left.pulsePosMinus();
+  } else {
+    left.pulsePosPlus();
+  }
+}
+
+void Chassis::isrRight() {
+  if (digitalRead(right.getPortB()) == 0) {
+    right.pulsePosMinus();
+  } else {
+    right.pulsePosPlus();
+  }
+}
+
+void Chassis::setup() {
+    left.reset(SLOT2);
+    right.reset(SLOT1);
+  // Set Pwm 8KHz
+  TCCR1A = _BV(WGM10);
+  TCCR1B = _BV(CS11) | _BV(WGM12);
+  TCCR2A = _BV(WGM21) | _BV(WGM20);
+  TCCR2B = _BV(CS21);
+  attachInterrupt(left.getIntNum(), isrLeft, RISING);
+  left.setPulse(9);
+  left.setRatio(39.267);
+  left.setPosPid(1.8, 0, 1.2);
+  left.setSpeedPid(0.18, 0, 0);
+  attachInterrupt(right.getIntNum(), isrRight, RISING);
+  right.setPulse(9);
+  right.setRatio(39.267);
+  right.setPosPid(1.8, 0, 1.2);
+  right.setSpeedPid(0.18, 0, 0);
+}
+
+void Chassis::setEffort(float leftEffort, float rightEffort) {
+    left.setMotorPwm(leftEffort);
+    right.setMotorPwm(-rightEffort);
+}
+
+void Chassis::loop() {
+    left.loop();
+    right.loop();
+}
+
+long Chassis::getLeftPos() {
+    return left.getCurPos();
+}
+
+long Chassis::getRightPos() {
+    return right.getCurPos();
+}
---
+++ src/Chassis.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <MeAuriga.h>
+
+class Chassis {
+ public:
+  Chassis();
+  /**
+   * Set the effort value for the left and right motors
+   */
+  void setEffort(float leftEffort, float rightEffort);
+
+  /**
+   * This method sets up the registers so the chassis motors will operate
+   * and needs to be called from the main program setup() function.
+   */
+  void setup();
+
+  /**
+   * This method should be called from the main program loop() function
+   * and ensures that the encoders operate properly. It needs to get
+   * moved somewhere else later.
+   */
+  void loop();
+
+  /**
+   * Get the left and right positions from the encoders. The units are
+   * in degrees of rotation.
+   */
+  long getLeftPos();
+  long getRightPos();
+
+ private:
+  static void isrLeft();
+  static void isrRight();
+};
